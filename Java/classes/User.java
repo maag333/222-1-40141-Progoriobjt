3b41137b18e2d2cbb15a7cf564effb082378f48e
administrador <administrador@imacdeastrador5.univa.mx>
2021-10-26 20:02:06 -0500

    segunda seccion java

---
+++ Java/classes/User.java
@@ -0,0 +1,18 @@
+public class User {
+
+   int id;
+   String full_name;
+   String personal_email;
+
+   //this is contructor
+   public User(int id, String full_name, String personal_email){
+       this.id = id;
+       this.full_name = full_name;
+       this.personal_email = personal_email;
+
+
+   }
+   
+
+
+}
